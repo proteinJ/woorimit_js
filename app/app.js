be1d4b61e9672101134976b3e3676941d64911cf
unknown <jaehyun516@naver.com>
2025-02-24 20:13:04 +0900

    Reop새로만들고 처음 올림

---
+++ app/app.js
@@ -0,0 +1,13 @@
+"use strict";
+const express = require('express');
+const app = express();
+
+// 앱 세팅
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static(`${__dirname}/src/public`));
+
+const home = require("./src/routes/home");
+app.use("/", home);    // use -> 미들웨어를 등록해주는 메서드.
+
+module.exports = app;
