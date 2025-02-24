3f112a8ac58b12476f0e2529f90f2998247fa50e
unknown <jaehyun516@naver.com>
2025-02-24 21:11:21 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
 "use strict";
+
+
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
+
 
 // 앱 세팅
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const home = require("./src/routes/home");
 app.use("/", home);    // use -> 미들웨어를 등록해주는 메서드.
