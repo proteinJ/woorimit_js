c385410dd8cb49841bf733571715c595c94a0b60
unknown <jaehyun516@naver.com>
2025-02-25 14:44:56 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,7 +2,7 @@
 
 
 const UserStorage = require("../../models/UserStorage");
- 
+const User = require("../../models/User");
 const output = { 
     home : (req, res) => {
         res.render("home/index");
@@ -14,23 +14,29 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
+     const user = new User(req.body);
+     const response = user.login();
+     return res.json(response);
 
-  const users = UserStorage.getUsers("id", "psword");
 
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
 
-        response.success = false;
-        response.msg = "로그인에 실패했습니다.";
-        return res.json(response);
+//         const id = req.body.id,
+//             psword = req.body.psword;
+
+//   const users = UserStorage.getUsers("id", "psword");
+
+//         const response = {};
+//         if(users.id.includes(id)) {
+//             const idx = users.id.indexOf(id);
+//             if(users.psword[idx] === psword) {
+//                 response.success = true;
+//                 return res.json(response);
+//             }
+//         }
+
+//         response.success = false;
+//         response.msg = "로그인에 실패했습니다.";
+//         return res.json(response);
         },
     };
 
@@ -38,4 +44,4 @@ const process = {
 module.exports = {
     output,
     process,
-}
+};
