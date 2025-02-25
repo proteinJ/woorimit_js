22e681ba384a579519c8d917faa51438624994c3
unknown <jaehyun516@naver.com>
2025-02-25 15:27:05 +0900

    로그인 화면 꾸미기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -18,25 +18,6 @@ const process = {
      const response = user.login();
      return res.json(response);
 
-
-
-//         const id = req.body.id,
-//             psword = req.body.psword;
-
-//   const users = UserStorage.getUsers("id", "psword");
-
-//         const response = {};
-//         if(users.id.includes(id)) {
-//             const idx = users.id.indexOf(id);
-//             if(users.psword[idx] === psword) {
-//                 response.success = true;
-//                 return res.json(response);
-//             }
-//         }
-
-//         response.success = false;
-//         response.msg = "로그인에 실패했습니다.";
-//         return res.json(response);
         },
     };
 
